43f814acf629e482391da5ed7a644de15f9abe5d
Yasha Mostofi <yasha.mostofi@gmail.com>
2017-06-24 21:26:52 -0700

    OAuth stuff

--- popup.js
+++ popup.js
@@ -1,8 +1,3 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
 function getCurrentTabUrl(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
@@ -10,23 +5,10 @@ function getCurrentTabUrl(callback) {
     active: true,
     currentWindow: true
   };
-
   chrome.tabs.query(queryInfo, function(tabs) {
-
     var tab = tabs[0];
-
     var url = tab.url;
-
-
     console.assert(typeof url == 'string', 'tab.url should be a string');
-
     callback(url);
   });
-
 }
-
-
-
-
-
-
